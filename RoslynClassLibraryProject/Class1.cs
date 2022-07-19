0d0be2d46bf2990544e7bd185cf8a26bdc3e1744
libra34567 <libra.li@hotmail.com>
2022-07-20 00:27:59 +0800

    install analysis package

--- RoslynClassLibraryProject/Class1.cs
+++ RoslynClassLibraryProject/Class1.cs
@@ -1,8 +1,39 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.Text;
 
-namespace RoslynClassLibraryProject
+namespace ExampleSourceGenerator
 {
-    public class Class1
+    [Generator]
+    public class ExampleSourceGenerator : ISourceGenerator
     {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            System.Console.WriteLine(System.DateTime.Now.ToString());
+
+            var sourceBuilder = new StringBuilder(
+            @"
+            using System;
+            namespace ExampleSourceGenerated
+            {
+                public static class ExampleSourceGenerated
+                {
+                    public static string GetTestText() 
+                    {
+                        return ""This is from source generator ");
+
+            sourceBuilder.Append(System.DateTime.Now.ToString());
+
+            sourceBuilder.Append(
+                @""";
+                    }
+    }
+}
+");
+
+            context.AddSource("exampleSourceGenerator", SourceText.From(sourceBuilder.ToString(), Encoding.UTF8));
+        }
+
+        public void Initialize(GeneratorInitializationContext context) { }
     }
 }
