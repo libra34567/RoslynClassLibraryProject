4103942f550c39d0acdd9f3321c65f6b5b469ee6
Andrii Kurdiumov <kant2002@gmail.com>
2022-07-25 22:58:56 +0600

    Rename to LittleToy

---
+++ LittleToySourceGenerator/LittleToySourceGenerator.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.Text;
+
+namespace LittleToySourceGenerator;
+
+[Generator]
+public class Generator : ISourceGenerator
+{
+    public void Execute(GeneratorExecutionContext context)
+    {
+        System.Console.WriteLine(System.DateTime.Now.ToString());
+
+        var sourceBuilder = new StringBuilder(
+        @"
+        using System;
+        namespace ToyMe
+        {
+            public static class ToyMeGenerated
+            {
+                public static string GetTestText() 
+                {
+                    return ""This is from source generator ");
+
+        sourceBuilder.Append(System.DateTime.Now.ToString());
+
+        sourceBuilder.Append(
+                @""";
+                }
+    }
+}
+");
+
+        context.AddSource("exampleSourceGenerator", SourceText.From(sourceBuilder.ToString(), Encoding.UTF8));
+    }
+
+    public void Initialize(GeneratorInitializationContext context) { }
+}
