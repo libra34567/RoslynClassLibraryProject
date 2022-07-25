77126b222ef9701bbff57f777bd51f76f6178af2
Andrii Kurdiumov <kant2002@gmail.com>
2022-07-26 00:27:09 +0600

    Add basuc event generator

---
+++ LittleToySourceGenerator.Tests/CodeGenerationTestBase.cs
@@ -0,0 +1,46 @@
+﻿namespace LittleToySourceGenerator.Tests;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+public class CodeGenerationTestBase
+{
+    protected string GetGeneratedOutput(string source, Generator generator, NullableContextOptions nullableContextOptions)
+    {
+        var syntaxTree = CSharpSyntaxTree.ParseText(source);
+
+        var references = new List<MetadataReference>();
+        Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+        foreach (var assembly in assemblies)
+        {
+            if (!assembly.IsDynamic && !string.IsNullOrWhiteSpace(assembly.Location))
+            {
+                references.Add(MetadataReference.CreateFromFile(assembly.Location));
+            }
+        }
+
+        var compilation = CSharpCompilation.Create(
+            "foo",
+            new SyntaxTree[] { syntaxTree },
+            references,
+            new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, nullableContextOptions: nullableContextOptions));
+
+        // var compileDiagnostics = compilation.GetDiagnostics();
+        // Assert.IsFalse(compileDiagnostics.Any(d => d.Severity == DiagnosticSeverity.Error), "Failed: " + compileDiagnostics.FirstOrDefault()?.GetMessage());
+
+        var driver = CSharpGeneratorDriver.Create(generator);
+        driver.RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out var generateDiagnostics);
+        Assert.IsFalse(generateDiagnostics.Any(d => d.Severity == DiagnosticSeverity.Error), "Failed: " + generateDiagnostics.FirstOrDefault()?.GetMessage());
+
+        string output = outputCompilation.SyntaxTrees.Last().ToString();
+
+        Console.WriteLine(output);
+
+        return output;
+    }
+}
