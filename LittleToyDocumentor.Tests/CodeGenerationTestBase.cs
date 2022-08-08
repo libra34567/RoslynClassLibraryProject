fa0141b28a1e405e6734ecaf2c8144ca25e67ee4
Andrii Kurdiumov <kant2002@gmail.com>
2022-08-08 15:07:52 +0600

    Simplest prototype for source generator Current source generator just find candidate calls to AddComponent and print count of them in the comments. But it should minimal amount of work, require to setup trivial source gen 1. Setup SyntaxReceiver which find appropriate syntax constructs 2. Process these constructs (not shown) 3. Generate source code using CsCodeGenerator (very trivial)

---
+++ LittleToyDocumentor.Tests/CodeGenerationTestBase.cs
@@ -0,0 +1,96 @@
+﻿namespace LittleToyDocumentor.Tests;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+public class CodeGenerationTestBase
+{
+    private static readonly MetadataReference CorlibReference = MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
+
+    protected string GetGeneratedOutput(string source, Generator generator, NullableContextOptions nullableContextOptions)
+    {
+        CSharpCompilation compilation = CreateCompilation(source, nullableContextOptions);
+
+        // var compileDiagnostics = compilation.GetDiagnostics();
+        // Assert.IsFalse(compileDiagnostics.Any(d => d.Severity == DiagnosticSeverity.Error), "Failed: " + compileDiagnostics.FirstOrDefault()?.GetMessage());
+
+        var driver = CSharpGeneratorDriver.Create(generator);
+        driver.RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out var generateDiagnostics);
+        Assert.IsFalse(generateDiagnostics.Any(d => d.Severity == DiagnosticSeverity.Error), "Failed: " + generateDiagnostics.FirstOrDefault()?.GetMessage());
+
+        string output = outputCompilation.SyntaxTrees.Last().ToString();
+
+        Console.WriteLine(output);
+
+        return output;
+    }
+
+    protected IEnumerable<Diagnostic> GetDiagnosticsFromGenerator(string source, Generator generator, NullableContextOptions nullableContextOptions)
+    {
+        CSharpCompilation compilation = CreateCompilation(source, nullableContextOptions);
+
+        // var compileDiagnostics = compilation.GetDiagnostics();
+        // Assert.IsFalse(compileDiagnostics.Any(d => d.Severity == DiagnosticSeverity.Error), "Failed: " + compileDiagnostics.FirstOrDefault()?.GetMessage());
+
+        var driver = CSharpGeneratorDriver.Create(generator);
+        driver.RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out var generateDiagnostics);
+        return generateDiagnostics;
+    }
+
+    private static CSharpCompilation CreateCompilation(string source, NullableContextOptions nullableContextOptions)
+    {
+        var fakeCode = @"
+using System;
+
+namespace Plugins.basegame.Events
+{
+    using DOTSNET;
+
+    [System.AttributeUsage(AttributeTargets.Struct)]
+    public class CodeGenNetComponentAttribute : System.Attribute
+    {
+        public readonly SyncDirection SyncDirection;
+
+        public CodeGenNetComponentAttribute(SyncDirection syncDirection)
+        {
+            SyncDirection = syncDirection;
+        }
+    }
+}
+";
+        List<MetadataReference> returnList = new();
+        returnList.Add(CorlibReference);
+
+        var fakeCompilation = CSharpCompilation.Create(
+            "shared",
+            new SyntaxTree[] { CSharpSyntaxTree.ParseText(fakeCode) },
+            returnList,
+            new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, nullableContextOptions: nullableContextOptions));
+
+        var syntaxTree = CSharpSyntaxTree.ParseText(source);
+
+        var references = new List<MetadataReference>();
+        Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+        foreach (var assembly in assemblies)
+        {
+            if (!assembly.IsDynamic && !string.IsNullOrWhiteSpace(assembly.Location))
+            {
+                references.Add(MetadataReference.CreateFromFile(assembly.Location));
+            }
+        }
+
+        references.Add(fakeCompilation.ToMetadataReference());
+
+        var compilation = CSharpCompilation.Create(
+            "foo",
+            new SyntaxTree[] { syntaxTree },
+            references,
+            new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, nullableContextOptions: nullableContextOptions));
+        return compilation;
+    }
+}
---
+++ LittleToyDocumentor/CsCodeGenerator/FileModel.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CsCodeGenerator
+{
+    public class FileModel
+    {
+        public FileModel() { }
+        public FileModel(string name)
+        {
+            Name = name;
+        }
+
+        public List<string> UsingDirectives { get; set; } = new List<string>();
+
+        public string Namespace { get; set; }
+
+        public string Name { get; set; }
+
+        public string Header { get; set; }
+
+        public string Extension { get; set; } = Util.CsExtension;
+
+        public string FullName => Name + "." + Extension;
+
+        public List<EnumModel> Enums { get; set; } = new List<EnumModel>();
+
+        public List<ClassModel> Classes { get; set; } = new List<ClassModel>();
+        
+        public List<StructModel> Structs { get; set; } = new List<StructModel>();
+        
+        public List<InterfaceModel> Interfaces { get; set; } = new List<InterfaceModel>();
+
+        public void LoadUsingDirectives(List<string> usingDirectives)
+        {
+            foreach (var usingDirective in usingDirectives)
+            {
+                UsingDirectives.Add(usingDirective);
+            }
+        }
+
+        public override string ToString()
+        {
+            string usingText = UsingDirectives.Count > 0 ? Util.Using + " " : "";
+            var headerText = !string.IsNullOrWhiteSpace(Header) ? Header + Util.NewLine : "";
+            string result = headerText + usingText + String.Join(Util.NewLine + usingText, UsingDirectives);
+            //result += string.IsNullOrEmpty(Namespace) ? "" : Util.NewLineDouble + Util.Namespace + " " + Namespace;
+            //result += Util.NewLine + "{";
+            if (string.IsNullOrEmpty(Namespace))
+            {
+                Enums.ForEach(ReduceIndent);
+                Classes.ForEach(ReduceIndent);
+                Structs.ForEach(ReduceIndent);
+                Interfaces.ForEach(ReduceIndent);
+            }
+
+            result += Util.NewLine;
+            result += string.Join(Util.NewLine, GetSourceElements());
+            //result += Util.NewLine + "}";
+            result += Util.NewLine;
+            return result;
+        }
+
+        private IEnumerable<string> GetSourceElements()
+        {
+            foreach (var o in Enums)
+            {
+                if (o != null)
+                {
+                    yield return o.ToString();
+                }
+            }
+            foreach (var o in Classes)
+            {
+                if (o != null)
+                {
+                    yield return o.ToString();
+                }
+            }
+            foreach (var o in Structs)
+            {
+                if (o != null)
+                {
+                    yield return o.ToString();
+                }
+            }
+            foreach (var o in Interfaces)
+            {
+                if (o != null)
+                {
+                    yield return o.ToString();
+                }
+            }
+        }
+
+        private static void ReduceIndent(BaseElement element)
+        {
+            if (element != null)
+            {
+                element.IndentSize -= CsGenerator.DefaultTabSize;
+            }
+        }
+
+        private static void ReduceIndent(InterfaceModel element)
+        {
+            ReduceIndent((BaseElement)element);
+            element?.Methods.ForEach(ReduceIndent);
+        }
+
+        private static void ReduceIndent(ClassModel element)
+        {
+            ReduceIndent((BaseElement)element);
+            element?.Methods.ForEach(ReduceIndent);
+            element?.Properties.ForEach(ReduceIndent);
+            element?.Fields.ForEach(ReduceIndent);
+        }
+
+        private static void ReduceIndent(StructModel element)
+        {
+            ReduceIndent((BaseElement)element);
+            element?.Methods.ForEach(ReduceIndent);
+            element?.Properties.ForEach(ReduceIndent);
+            element?.Fields.ForEach(ReduceIndent);
+        }
+    }
+}
