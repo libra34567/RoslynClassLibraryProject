a604b037e8b7ae17f90a54e3cbc9f93f02886190
Andrii Kurdiumov <kant2002@gmail.com>
2022-08-08 18:31:16 +0600

    Add prototype doc generation

--- LittleToyDocumentor/Generator.cs
+++ LittleToyDocumentor/Generator.cs
@@ -4,7 +4,9 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace LittleToyDocumentor;
@@ -29,9 +31,12 @@ public class Generator : ISourceGenerator
             return;
         }
 
-        var file = new FileModel("Documenation")
+        var symbols = GetSymbols(context.Compilation, receiver.MemberAccessExpressionSyntaxes).Distinct().ToList();
+        foreach (var typeInformation in symbols.GroupBy(s => s.Type))
         {
-            UsingDirectives = new List<string>
+            var file = new FileModel(typeInformation.Key.Name + "Documentation")
+            {
+                UsingDirectives = new List<string>
             {
                 "System;",
                 "Unity.Entities;",
@@ -40,14 +45,104 @@ public class Generator : ISourceGenerator
                 "DOTSNET;",
                 "Unity.Collections;"
             },
-            Header = FileHeader,
-        };
-        file.Classes.Add(new ClassModel("DocumentationClass")
+                Header = FileHeader,
+                Namespace = typeInformation.Key.ContainingNamespace?.Name ?? "",
+            };
+            StringBuilder comment = new StringBuilder();
+            bool firstOperation = true;
+            foreach (var operationInformation in typeInformation.GroupBy(ti => ti.OperationName))
+            {
+                if (!firstOperation)
+                {
+                    comment.AppendLine();
+                }
+
+                bool firstLine = true;
+                var callSites = operationInformation.Select(_ => (_.Method.Name, _.Method.ReceiverType));
+                foreach (var callSiteInformation in callSites.OrderBy(type => type.ReceiverType.Name).GroupBy(_ => _.ReceiverType))
+                {
+                    var usedMethods = callSiteInformation.Select(_ => _.Name).OrderBy(_ => _);
+                    if (firstLine)
+                    {
+                        comment.AppendLine($"{operationInformation.Key} in {string.Join(", ", usedMethods)} of {callSiteInformation.Key.Name}");
+                        firstLine = false;
+                    }
+                    else
+                    {
+                        comment.AppendLine($"     in {string.Join(", ", usedMethods)} of {callSiteInformation.Key.Name}");
+                    }
+                }
+
+                firstOperation = false;
+            }
+
+            file.Classes.Add(new ClassModel(typeInformation.Key.Name)
+            {
+                Comment = comment.ToString(),
+                UseXmlDocCommentStyle = true,
+                SingleKeyWord = KeyWord.Partial,
+            });
+            context.AddSource(file.Name, SourceText.From(file.ToString(), Encoding.UTF8));
+        }
+    }
+
+    private IEnumerable<(/*MemberAccessExpressionSyntax SyntaxNode, */string OperationName, IMethodSymbol Method, ITypeSymbol Type)> GetSymbols(Compilation compilation, IList<MemberAccessExpressionSyntax> syntaxes)
+    {
+        foreach (var target in syntaxes)
         {
-            Comment = $"Found {receiver.MemberAccessExpressionSyntaxes.Count} candidates",
-            SingleKeyWord = KeyWord.Partial,
-        });
-        context.AddSource(file.Name, SourceText.From(file.ToString(), Encoding.UTF8));
+            var model = compilation.GetSemanticModel(target.SyntaxTree);
+            var methodSymbol = model.GetSymbolInfo(target).Symbol as IMethodSymbol;
+            if (!IsValidMethod(methodSymbol))
+            {
+                continue;
+            }
+            
+            if (methodSymbol.IsGenericMethod)
+            {
+                var operationName = methodSymbol.Name.StartsWith("Add") ? "Added" : "Removed";
+                var componentType = methodSymbol.TypeArguments[0];
+                var methodDeclaration = FindMethodDeclaration(target);
+                methodSymbol = model.GetDeclaredSymbol(methodDeclaration);
+                yield return (/*target, */operationName, methodSymbol, componentType);
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }    
+    }
+
+    private static MethodDeclarationSyntax FindMethodDeclaration(SyntaxNode node)
+    {
+        while (node != null)
+        {
+            if (node is MethodDeclarationSyntax method)
+            {
+                return method;
+            }
+
+            node = node.Parent;
+        }
+
+        return null;
+    }
+
+    private static bool IsValidMethod(IMethodSymbol methodSymbol)
+    {
+        var type = methodSymbol.ReceiverType;
+        var wellKnownTypes = new[] { "Unity.Entities.EntityManager", "Unity.Entities.EntityCommandBuffer", "Unity.Entities.EntityCommandBuffer.ParallelWriter" };
+        if (!wellKnownTypes.Contains(type.ToDisplayString()))
+        {
+            return false;
+        }
+
+        var wellKnownMethods = new[] { "AddComponent", "AddComponentData", "RemoveComponent", "RemoveComponentData" };
+        if (!wellKnownMethods.Contains(methodSymbol.Name))
+        {
+            return false;
+        }
+
+        return true;
     }
 
     /// <inheritdoc/>
@@ -66,7 +161,8 @@ public class Generator : ISourceGenerator
             {
                 if (invocationExpressionSyntax.Expression is MemberAccessExpressionSyntax memberAccessExpressionSyntax)
                 {
-                    if (memberAccessExpressionSyntax.OperatorToken.IsKind(SyntaxKind.DotToken) && memberAccessExpressionSyntax.Name.ToFullString().StartsWith("AddComponent"))
+                    if (memberAccessExpressionSyntax.OperatorToken.IsKind(SyntaxKind.DotToken) && 
+                        (memberAccessExpressionSyntax.Name.ToFullString().StartsWith("AddComponent") || memberAccessExpressionSyntax.Name.ToFullString().StartsWith("RemoveComponent")))
                     {
                         MemberAccessExpressionSyntaxes.Add(memberAccessExpressionSyntax);
                     }
