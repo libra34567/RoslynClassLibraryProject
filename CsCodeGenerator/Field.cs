93cd7ebdcae92a4498b924a6d70d70c5dd621199
Andrii Kurdiumov <kant2002@gmail.com>
2022-08-10 17:28:14 +0600

    Add mising ReadOnly attribute

--- CsCodeGenerator/Field.cs
+++ CsCodeGenerator/Field.cs
@@ -19,8 +19,6 @@ namespace CsCodeGenerator
         public virtual string DefaultValue { get; set; }
         protected string DefaultValueFormated => DefaultValue != null ? " = " + DefaultValue : "";
 
-        public override bool HasAttributes => false;
-
         protected virtual string Ending { get; } = ";";
 
         public override string ToString() => base.ToString() + Body + DefaultValueFormated + Ending;
